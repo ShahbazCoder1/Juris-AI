c018293c8341ffbbe56bae87360b102b0e726f6b
shahbazhashmi14a <shahbazhashmi14a@gmail.com>
2024-10-04 03:02:56 +0000

    Responsiveness improved. Now Website can be opended in mobile view.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuToggle.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+  });
+});
+
 document.getElementById('chat-button').addEventListener('click', function () {
   document.getElementById('chat-box').style.display = 'flex';  // Show the chat box
 });
